9eee0fdc000c1f4b7b55854139dd28ab1404f94d
Dmytro Ohanov <dmitryohanovphotography@gmail.com>
2024-03-28 22:57:17 +0000

    updated logic of checkMembership

--- src/middleware/checkMembership.ts
+++ src/middleware/checkMembership.ts
@@ -11,18 +11,18 @@ export const checkMembership = new Composer();
 checkMembership.on("message", async (ctx) => {
   const isMember = await ctx.api.getChatMember("@evrika_museum", ctx.chat.id);
 
-  if (isMember.status === "left") {
-    await ctx.reply(`${followText}`);
-  } else {
-    if (ctx.msg.text === "Пройти квіз 🤓") {
+  if (ctx.msg.text === "Пройти квіз 🤓") {
+    if (isMember.status === "left") {
+      await ctx.reply(`${followText}`);
+    } else {
       await ctx.reply(`${takeQuiz}`);
       setTimeout(() => {
         ctx.api.sendMessage(ctx.chat.id, `${socialNetworks}`);
       }, 1000 * 60);
-    } else if (ctx.msg.text === "Соц мережі 📊") {
-      await ctx.reply(`${socialNetworks}`);
-    } else {
-      await ctx.reply(`${noCommand}`);
     }
+  } else if (ctx.msg.text === "Соц мережі 📊") {
+    await ctx.reply(`${socialNetworks}`);
+  } else {
+    await ctx.reply(`${noCommand}`);
   }
 });
